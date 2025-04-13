401aea77829b595764bc40f30b3c3e4a6969adbe
HoangKhang <nguyenhoangkhq7@gmail.com>
2025-04-13 09:10:43 +0700

    lab06 - 7. SPA

--- lab06/src/App.jsx
+++ lab06/src/App.jsx
@@ -1,6 +1,13 @@
 import "./App.css";
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Teams from "./pages/Teams";
+import Analytics from "./pages/Analytics";
+import Messages from "./pages/Messages";
+import Integrations from "./pages/Integrations";
 
 function App() {
   return (
@@ -9,7 +16,14 @@ function App() {
         <Sidebar />
       </div>
       <div className="col-span-9">
-        <Dashboard />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/integrations" element={<Integrations />} />
+        </Routes>
       </div>
     </div>
   );
